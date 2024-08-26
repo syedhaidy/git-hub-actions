e35babad7507db056352adc4b145dccd1bf4a7a5
syedhaider <syedhaider@fico.com>
2024-08-26 15:10:15 +0530

    (17-custom action JS): Parse and validate the inputs

--- .github/actions/jss-dependency-update/index.js
+++ .github/actions/jss-dependency-update/index.js
@@ -1,27 +1,81 @@
 const core = require('@actions/core');
- 
-async function run() { 
+const exec = require('@actions/exec');
+
+const validateBranchName = ({ branchName }) =>
+  /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
+const validateDirectoryName = ({ dirName }) =>
+  /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
+
+async function run() {
+  const baseBranch = core.getInput('base-branch');
+  const targetBranch = core.getInput('target-branch');
+  const ghToken = core.getInput('gh-token');
+  const workingDir = core.getInput('working-directory');
+  const debug = core.getBooleanInput('debug');
+
+  core.setSecret(ghToken);
+
+  if (!validateBranchName({ branchName: baseBranch })) {
+    core.setFailed(
+      'Invalid base-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.'
+    );
+    return;
+  }
+
+  if (!validateBranchName({ branchName: targetBranch })) {
+    core.setFailed(
+      'Invalid target-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.'
+    );
+    return;
+  }
+
+  if (!validateDirectoryName({ dirName: workingDir })) {
+    core.setFailed(
+      'Invalid working directory name. Directory names should include only characters, numbers, hyphens, underscores, and forward slashes.'
+    );
+    return;
+  }
+
+  core.info(`[js-dependency-update] : base branch is ${baseBranch}`);
+  core.info(`[js-dependency-update] : target branch is ${targetBranch}`);
+  core.info(`[js-dependency-update] : working directory is ${workingDir}`);
+
+  await exec.exec('npm update', [], {
+    cwd: workingDir,
+  });
+
+  const gitStatus = await exec.getExecOutput(
+    'git status -s package*.json',
+    [],
+    {
+      cwd: workingDir,
+    }
+  );
+
+  if (gitStatus.stdout.length > 0) {
+    core.info('[js-dependency-update] : There are updates available!');
+  } else {
+    core.info('[js-dependency-update] : No updates at this point in time.');
+  }
   /*
   What they Actually do:
     @actions/exec - Utility to execute the commandline script.
     @actions/github - Interact with Github API
 
     In this scenario, whenver there is change in package.json file we will raise PR.
-    1.Parse Input
-      1.1 base-branch form which check for  updates
-      1.2 Target Branch to use create the PR
-      1.3 Git Hub token for authentication purpose
-      1.4 Working Directory for which we check for dependencies 
-    2. Execute the npm update command inside the working directory
-        2.1 in our case is 14-artifacts/reac-app, run npm update
-           1469  Packege are added
-    3. Check whether there are modifieed files, create PR 
-    4. If there are modified files, create PR
-      4.1 Add and commit the file to target-branch
-      4.2 Create PR to base branch using  octokit API
-    5. Otherwise , conclude the custom actions 
-  */
+  [DONE] 1. Parse inputs:
+    1.1 base-branch from which to check for updates
+    1.2 target-branch to use to create the PR
+    1.3 Github Token for authentication purposes (to create PRs)
+    1.4 Working directory for which to check for dependencies
+  [DONE] 2. Execute the npm update command within the working directory
+  [DONE] 3. Check whether there are modified package*.json files
+  4. If there are modified files:
+    4.1 Add and commit files to the target-branch
+    4.2 Create a PR to the base-branch using the octokit API
+  5. Otherwise, conclude the custom action
+   */
   core.info('I am a custom JS action');
 }
- 
+
 run();
